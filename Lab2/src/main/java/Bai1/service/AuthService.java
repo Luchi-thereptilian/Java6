f18609d442aea73dc7237b0526439dee7fb35cb1
Luchi <luyiddo@gmail.com>
2025-11-04 19:00:49 +0700

    update 2 bai lab 2 , 1 bai nua la xong

---
+++ Lab2/src/main/java/Bai1/service/AuthService.java
@@ -0,0 +1,28 @@
+package Bai1.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Service("auth")
+public class AuthService{
+    public Authentication getAuthentication(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+    public String getUsername(){
+        return getAuthentication().getName();
+    }
+    public String getRoles(){
+        return getAuthentication().getAuthorities().iterator().next().getAuthority();
+    }
+    public boolean isAuthenticated(){
+        String Username = this.getUsername();
+        return (Username!=null && !Username.equals("anonymousUser"));
+    }
+    public boolean hasAnyRole(String... rolesToCheck){
+        var grantedRoles = this.getRoles();
+        return Stream.of(rolesToCheck).anyMatch(role -> grantedRoles.contains(role));
+    }
+}
---
+++ Lab2/src/main/java/Bai2/config/Security.java
@@ -0,0 +1,72 @@
+package Bai2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Bỏ cấu hình mặc định CSRF và CORS
+        http.csrf(config -> config.disable()).cors(config -> config.disable());
+// Phân quyền sử dụng
+        http.authorizeHttpRequests(config -> {
+//            config.requestMatchers("/poly/**").authenticated(); ( tat cai nay de ap dung roll)
+            config.requestMatchers("poly/url0").hasRole("USER");
+            config.requestMatchers("poly/url1").hasRole("ADMIN");
+            config.requestMatchers("poly/url2").hasAnyRole("USER","ADMIN");
+            config.requestMatchers("poly/url3").hasRole("USER");
+            config.requestMatchers("poly/url4").hasRole("ADMIN");
+            config.anyRequest().permitAll();
+        });
+// Form đăng nhập mặc định
+        http.formLogin(config ->{
+            config.loginPage("/login/form");
+            config.loginProcessingUrl("/login/check");
+            config.defaultSuccessUrl("/login/success");
+            config.failureUrl("/login?failure");
+            config.permitAll();
+            config.usernameParameter("username");
+            config.passwordParameter("password");
+                }
+                );
+// Ghi nhớ tài khoản
+        http.rememberMe(config -> {
+            config.tokenValiditySeconds(3*24*60*60);
+            config.rememberMeParameter("remember-me");
+            config.rememberMeCookieName("rememberMe");
+        });
+// Đăng xuất
+        http.logout(config -> {
+            config.logoutUrl("/logout");
+            config.logoutSuccessUrl("/login/exit");
+            config.clearAuthentication(true);
+            config.invalidateHttpSession(true);
+            config.deleteCookies("remember-me");
+        });
+        return http.build();
+    }
+}
